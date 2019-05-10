c4223a33c448f31b61799257cef1e6f00620c365
maycon da silva moreira <maycondasilvamoreira@hotmail.com>
2019-05-10 10:45:53 -0300

    configurado arquivo de rotas

--- ProvaSquadraLaravel/routes/web.php
+++ ProvaSquadraLaravel/routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//nome, apelido,controller, metodo
+Route::get('/',['as'=>'home', 'uses'=>'HomeController@index']);
+Route::get('/sistema/buscar',['as'=>'sistema.buscar', 'uses'=>'SistemaController@buscar']);
+Route::get('/sistema/adicionar',['as'=>'sistema.adicionar','uses'=>'SistemaController@adicionar']);
+Route::post('/sistema/salvar',['as'=>'sistema.salvar','uses'=>'SistemaController@salvar']);
+Route::get('/sistema/editar/{id}',['as'=>'sistema.editar', 'uses'=>'SistemaController@editar']);
+Route::put('/sistema/atualizar/{id}',['as'=>'sistema.atualizar', 'uses'=>'SistemaController@atualizar']);
